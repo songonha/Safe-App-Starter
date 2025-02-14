d24c2932bd26118acd7fbd355e9e7eef40eff96c
Songonha <105191398+songonha@users.noreply.github.com>
2025-02-14 15:28:08 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { Theme, ThemeProvider } from '@mui/material/styles'
 import CircularProgress from '@mui/material/CircularProgress'
 import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
 import { SafeThemeProvider } from '@safe-global/safe-react-components'
 import SafeProvider from '@safe-global/safe-apps-react-sdk'
 
@@ -20,9 +22,31 @@ root.render(
           <SafeProvider
             loader={
               <>
-                <Typography variant="h1">Waiting for Safe...</Typography>
-                <CircularProgress color="primary" />
+                <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh">
+                  <Typography textAlign="center" variant="h1">Waiting for Safe...</Typography>
+                  <CircularProgress color="primary" />
+             
+
+               
+                  <Typography variant="h4" textAlign="center" gutterBottom>
+                    How to mint MintNPass NFT on Safe{`{Wallet}`} for free?
+                  </Typography>
+                  <Link 
+                    href="https://www.youtube.com/watch?v=2ML_0lvoWn0" 
+                    color="green" 
+                    underline="hover"
+                    variant="h6"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Watch on YouTube
+                  </Link>
+                  
+                </Box>
+
               </>
+
+             
             }
           >
             <App />
